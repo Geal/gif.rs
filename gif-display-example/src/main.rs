a89b6c1820eeebc7858cae2c0806a9dce31b2348
Geoffroy Couprie <geo.couprie@gmail.com>
2015-12-07 14:52:39 +0100

    Update to latest glium

--- gif-display-example/src/main.rs
+++ gif-display-example/src/main.rs
@@ -1,129 +1,86 @@
-extern crate graphics;
+#[macro_use]
 extern crate glium;
-extern crate glutin;
-extern crate glium_graphics;
-extern crate image;
-extern crate sdl2_window;
-extern crate piston;
-
 extern crate gif;
 extern crate nom;
 
-use nom::IResult;
-use nom::util::*;
+
+use glium::{DisplayBuild, Surface};
+
+use glium::glutin;
+
+use nom::{IResult,HexDisplay};
+//use nom::util::*;
 use gif::*;
 use gif::parser::*;
 use gif::lzw::*;
 
-use std::rc::Rc;
-use std::cell::RefCell;
-use std::path::Path;
-use std::vec::Vec;
-use glium::{ Surface, Texture2d };
-use glium_graphics::{ Glium2d, GliumGraphics, DrawTexture, GliumWindow };
-use sdl2_window::{ Sdl2Window, OpenGL };
-use piston::event::*;
-use piston::window::{ WindowSettings, Size };
-
 fn main() {
-    println!("starting...");
-    let opengl = OpenGL::_3_2;
-    let (w, h) = (400, 300);
-    let ref window = Rc::new(RefCell::new(
-        Sdl2Window::new(
-            opengl,
-            WindowSettings::new(
-                "glium_graphics: image_test".to_string(),
-                Size { width: w, height: h }
-            ).exit_on_esc(true)
-        )
-    ));
-    let ref glium_window = GliumWindow::new(window).unwrap();
+  // building the display, ie. the main object
+  let display = glutin::WindowBuilder::new()
+    .with_dimensions(400, 300)
+    .with_vsync()
+    .build_glium()
+    .unwrap();
 
-    println!("A");
-    let rust_logo = DrawTexture::new({
-        let image = image::open(&Path::new("../assets/rust.png")).unwrap();
-        Texture2d::new(glium_window, image)
-    });
+  let decoded = decode_gif();
+  let mut reversed:Vec< Vec<(u8,u8,u8)> > = Vec::new();
+  for el in decoded.iter().rev() {
+    reversed.push(el.clone());
+  }
+  let dest_texture = glium::Texture2d::new(&display, reversed).unwrap();
 
-    println!("B");
-    let pixels = DrawTexture::new({
-        let decoded = decode_gif();
-        let mut reversed:Vec< Vec<(u8,u8,u8)> > = Vec::new();
-        for el in decoded.iter().rev() {
-          reversed.push(el.clone());
-        }
-        Texture2d::new(glium_window, reversed)
-    });
-    /*let pixels = DrawTexture::new({
-        Texture2d::new(&window,
-           vec![
-              vec![(0u8, 1u8, 2u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
-              vec![(32u8, 64u8, 128u8), (32u8, 16u8, 4u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
-              vec![(32u8, 64u8, 128u8), (32u8, 16u8, 4u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
-              vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)]
-        ])
-    });*/
+  /*
+  let dest_texture = glium::Texture2d::new(&display,
+  vec![
+  vec![(0u8, 1u8, 2u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
+  vec![(32u8, 64u8, 128u8), (32u8, 16u8, 4u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
+  vec![(32u8, 64u8, 128u8), (32u8, 16u8, 4u8), (4u8, 8u8, 16u8), (4u8, 8u8, 16u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)],
+  vec![(255u8, 64u8, 2u8), (10u8, 255u8, 255u8), (128u8, 8u8, 255u8), (4u8, 8u8, 255u8)]
+  ]).unwrap();
+  */
 
-    println!("C");
-    let mut g2d = Glium2d::new(OpenGL::_3_2, glium_window);
-    println!("w: {:?}, h: {:?}", w, h);
-    let transform = graphics::math::abs_transform(w as f64, h as f64);
-    println!("f: w: {:?}, h: {:?}", w as f64, h as f64);
+  // the main loop
+  loop {
+    // drawing a frame
+    let target = display.draw();
+    dest_texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Nearest);
+    target.finish().unwrap();
 
- 
-    for e in window.events().swap_buffers(false) {
-      if let Some(_) = e.render_args() {
-        let mut target = glium_window.draw();
-        {
-            use graphics::*;
-
-            //println!("E");
-            let mut g = GliumGraphics::new(&mut g2d, &mut target);
-
-            clear(color::WHITE, &mut g);
-            rectangle([1.0, 0.0, 0.0, 1.0],
-                      [0.0, 0.0, 100.0, 100.0],
-                      transform,
-                      &mut g);
-            rectangle([0.0, 1.0, 0.0, 0.3],
-                      [50.0, 50.0, 100.0, 100.0],
-                      transform,
-                      &mut g);
-            //println!("F");
-            image(&rust_logo, transform.trans(0.0, 0.0), &mut g);
-            image(&pixels, transform.trans(0.0, 0.0), &mut g);
-            //println!("G");
-
-        }
-        target.finish();
+    // polling and handling the events received by the window
+    for event in display.poll_events() {
+      match event {
+        glutin::Event::Closed => return,
+        _ => ()
       }
     }
+
+    std::thread::sleep(std::time::Duration::from_millis(50));
+  }
 }
 
 pub fn buf_to_colors(buffer: &[u8], row_size: usize) -> Vec< Vec<(u8,u8,u8)> > {
